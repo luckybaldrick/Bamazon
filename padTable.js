8a536143dc3a700fdd67a29f8fd7b01a7fd9fdbf
unknown <matthewhknetsch@gmail.com>
2018-07-17 14:26:39 -0600

    manager page

---
+++ padTable.js
@@ -0,0 +1,32 @@
+function padText(titleText, entryText){
+  
+
+    var padLeft;
+    var padRight;
+  
+ 
+    var splitLength = (titleText.length - entryText.length)/2;
+  
+ 
+    var pad = '';
+    for(var j=0; j < splitLength; j++){
+      pad += ' ';
+    }
+  
+
+    if(Number.isInteger(splitLength)){
+      padLeft = pad;
+      padRight = pad;
+    }
+    else{
+      padLeft = pad;
+      padRight = pad.substring(0, pad.length-1); // remove last space
+    }
+  
+ 
+    return padLeft + entryText + padRight;
+  }
+  
+  
+ 
+  module.exports = padText;
